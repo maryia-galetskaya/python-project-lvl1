8b8e1441d10df6f4eb8ff64d821a160882cd8199
Maryia Galetskaya <maryia.galetskaya@gmail.com>
2022-08-30 15:50:14 +0530

    k

--- brain_games/scripts/structure_of_all_games.py
+++ brain_games/scripts/structure_of_all_games.py
@@ -17,7 +17,6 @@ def welcome_user ():
     print (f'Hello, {name}!')
     return name
 
-
     
 def even (num):
     if num % 2 == 0:
@@ -35,29 +34,15 @@ def congrats (user_name):
 
 
 
-def brain_even (user_name):
-    print ('Answer "yes" if the number is even, otherwise answer "no".')
-    tries=3 
-    while tries:
-        this_number = number()
-        ask_answer = input (f'Question:{this_number}\nYour answer: ')
-        if even(this_number) == ask_answer: #or odd() == ask_answer:
-            print ('Correct!')
-            tries-=1
-        else: 
-            print (f"'{ask_answer}' is wrong answer ;(. Correct answer was '{(even(this_number))}'.\nLet's try again, {user_name}!")
-            return #f"Let's try again, {name}!"
-    else:
-        print (f'Congratulations, {user_name}!')
 
 
 #print (welcome_user())
 #print (brain_even())
 
 def main():  
-    
+    number ()
     user_name = welcome_user()
-    brain_even(user_name)
+    #brain_even(user_name)
 
 if __name__ == '__main__':
     main()
