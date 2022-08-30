41378c3a6933760939a4ce9b81904d279cfabe7f
Maryia Galetskaya <maryia.galetskaya@gmail.com>
2022-08-30 14:36:01 +0530

    add

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -7,56 +7,35 @@ def random_calc():
     rand_calc_str = (random.choice(plus_minus_mult))
     return rand_calc_str
 
-
-#def calc (num1, num2):
-    if random_calc() == '+':
-        return num1 + num2
-    if random_calc() == '-':
-        return num1 - num2 
-    if random_calc() == '*':
-        return num1 * num2
-
 def brain_calc (user_name):
     #welcome_user()
     print ('What is the result of the expression?')
     tries=3 
     while tries:
         number1 = number()
-        number2 = number() #and <number1
-        #result_sum = number1 + number2
-        #result_minus = number1 - number2
-        #result_mult = number1 * number2
+        number2 = number() 
+        
         sign = random_calc()
+        
         if sign == '+':
-            ask_answer = input (f'Question:{number1} + {number2}\nYour answer: ')
-            result_sum = number1 + number2
-            if ask_answer == str (result_sum):
-                print ('Correct!')
+            right_answer = str (number1 + number2)
+            
+            return right_answer
+        elif sign == '-':
+            right_answer = str (number1 - number2)
+            return right_answer
+        else: #sign == '*':
+            right_answer = str (number1 * number2)
 
-            else:
-                print (f"'{ask_answer}' is wrong answer ;(. Correct answer was '{result_sum}'.\nLet's try again, {user_name}!")
-
-            #return ask_answer
-        if random_calc() == '-':
-            ask_answer = input (f'Question:{number1} - {number2}\nYour answer: ')
-            result_minus = number1 - number2
-            if ask_answer == str (result_minus):
-                print ('Correct!')
-               
-            else: 
-                print (f"'{ask_answer}' is wrong answer ;(. Correct answer was '{result_minus}'.\nLet's try again, {user_name}!")
-                
-            #return ask_answer
-        if random_calc() == '*':
-            ask_answer = input (f'Question:{number1} * {number2}\nYour answer: ')
-            result_mult = number1 * number2
-            if ask_answer == str (result_mult):
-                print ('Correct!')
-                
-            else: 
-                print (f"'{ask_answer}' is wrong answer ;(. Correct answer was '{result_mult}'.\nLet's try again, {user_name}!")
-                
-            #return ask_answer
+        
+        ask_answer = input (f'Question:{number1} {sign} {number2}\nYour answer: ')
+        if ask_answer == right_answer:
+            print ('Correct!')
+            #tries-=1
+        else:
+            wrong_answer = (f"'{ask_answer}' is wrong answer ;(. Correct answer was '{right_answer}'.\nLet's try again, {user_name}!")
+            print (wrong_answer)
+            return wrong_answer
         tries-=1
     else:
         print (f'Congratulations, {user_name}!')
