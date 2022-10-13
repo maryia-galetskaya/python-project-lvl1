771ecfa0baeb15fecbedd6ca30f2f0c68720258e
maryia-galetskaya <110808709+maryia-galetskaya@users.noreply.github.com>
2022-10-13 16:31:25 +0530

    Update brain_progression.py

--- brain_games/scripts/games/brain_progression.py
+++ brain_games/scripts/games/brain_progression.py
@@ -2,7 +2,7 @@ from brain_games.scripts.structure_of_all_games import welcome_user, wrong_answe
 import random
 
 
-def length_of_pr ():
+def length_of_pr():
     min_len_of_row = 5
     max_len_of_row = 11
     return random.randint(min_len_of_row, max_len_of_row)
@@ -20,7 +20,7 @@ def start_of_row():
 
 def brain_progression(user_name):
     print('What number is missing in the progression?')
-    tries = 3 
+    tries = 3
     while tries:
         len_of_pr = length_of_pr()
         difference_in_row = difference_in_row_()
@@ -30,16 +30,16 @@ def brain_progression(user_name):
         while len(row) != len_of_pr:
             row.append(start_number)
             start_number += difference_in_row
-        hidden_index = random.randint(0, len(row)-1)
-        right_answer = row [hidden_index]
+        hidden_index = random.randint(0, len(row) - 1)
+        right_answer = row[hidden_index]
         row[hidden_index] = '..'
         row.insert(0, 'Question:')
 
         print(*row)
-        ask_answer = input('Your answer: ')  # 8) ask answer
-        if ask_answer == str(right_answer): # 9) got answer and compare to right one
+        ask_answer = input('Your answer: ')
+        if ask_answer == str(right_answer):
             print('Correct!')
-            tries-=1
+            tries -= 1
         else:
             wrong_answer (ask_answer, right_answer, user_name)
             break
@@ -51,5 +51,6 @@ def main():
     user_name = welcome_user()
     brain_progression(user_name)
 
+    
 if __name__ == '__main__':
     main()
