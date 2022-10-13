941733d7ff48a228363a33df1dbb05c638829931
maryia-galetskaya <110808709+maryia-galetskaya@users.noreply.github.com>
2022-10-13 17:17:53 +0530

    Update brain_calc.py

--- brain_games/scripts/games/brain_calc.py
+++ brain_games/scripts/games/brain_calc.py
@@ -6,7 +6,7 @@ import random
 
 def random_calc():
     plus_minus_mult = ('+', '-', '*')
-    rand_calc_str =(random.choice(plus_minus_mult))
+    rand_calc_str = (random.choice(plus_minus_mult))
     return rand_calc_str
 
 
@@ -17,7 +17,7 @@ def right_ans(number1, number2, sign):
     elif sign == '-':
         right_answer = str(number1 - number2)
         return right_answer
-    else: # sign == '*'
+    else:
         right_answer = str(number1 * number2)
         return right_answer
 
@@ -26,11 +26,11 @@ def brain_calc(user_name):
     print('What is the result of the expression?')
     tries = 3
     while tries:
-        number1 = random_number()
-        number2 = random_number()
+        num1 = random_number()
+        num2 = random_number()
         sign = random_calc()
-        ask_answer = input(f'Question: {number1} {sign} {number2}\nYour answer: ')
-        right_answer = right_ans(number1, number2, sign)
+        ask_answer = input(f'Question: {num1} {sign} {num2}\nYour answer: ')
+        right_answer = right_ans(num1, num2, sign)
 
         if ask_answer == right_answer:
             print('Correct!')
@@ -46,6 +46,6 @@ def main():
     user_name = welcome_user()
     brain_calc(user_name)
 
-    
+
 if __name__ == '__main__':
     main()
