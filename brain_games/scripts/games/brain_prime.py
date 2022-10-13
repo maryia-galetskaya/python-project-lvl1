4a141964be763925fa7aca63f32adb5d13e63aea
maryia-galetskaya <110808709+maryia-galetskaya@users.noreply.github.com>
2022-10-13 16:27:13 +0530

    Update brain_prime.py

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -1,10 +1,11 @@
-from brain_games.scripts.structure_of_all_games import welcome_user, wrong_answer, congrats, random_number
+from brain_games.scripts.structure_of_all_games 
+import welcome_user, wrong_answer, congrats, random_number
 
 
 def isPrime(number):
     if number == 1:
         return 'yes'
-    if number <1:
+    if number < 1:
         return 'Number is negative'
     i = 2
     while i < number:
@@ -22,9 +23,9 @@ def brain_prime(user_name):
         asked_number = random_number()
         right_answer = isPrime(asked_number)
         ask_answer = input(f'Question: {asked_number}\nYour answer: ')
-        if ask_answer == str(right_answer): 
+        if ask_answer == str(right_answer):
             print('Correct!')
-            tries-=1
+            tries -= 1
         else:
             wrong_answer(ask_answer, right_answer, user_name)
             break
@@ -36,5 +37,6 @@ def main():
     user_name = welcome_user()
     brain_prime(user_name)
 
+    
 if __name__ == '__main__':
     main()
